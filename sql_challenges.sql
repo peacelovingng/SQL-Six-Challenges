7cc92aff064257a72ae74135fc48162878010a36
Olawale Ahmed Alamu <alamuolawale@gmail.com>
2024-12-12 16:56:18 +0100

    Added Challenge 2

--- sql_challenges.sql
+++ sql_challenges.sql
@@ -1,3 +1,7 @@
+/*1. Ranking, Dense Ranking, and Row Number
+Write a query to rank customers based on the total amount they have spent on orders. 
+Provide Rank, Dense Rank,  and Row Number for each customer.*/
+
 WITH customers_spending AS (
     SELECT customer_id, customer_name, SUM(order_amount) as total_spent
     FROM customers
@@ -7,8 +11,11 @@ WITH customers_spending AS (
 )
 
 SELECT customer_name, total_spent,
-ROW_NUMBER() OVER(ORDER BY total_spent DESC) AS row_number
-RANK() OVER(ORDER BY total_spent DESC) AS rank
+ROW_NUMBER() OVER(ORDER BY total_spent DESC) AS row_number,
+RANK() OVER(ORDER BY total_spent DESC) AS rank,
 DENSE_RANK() OVER(ORDER BY total_spent DESC) AS dense_rank 
 FROM customers_spending;
+
+/*2. Cumulative Summations
+Calculate the cumulative sum of the order amounts for each customer.*/
 
